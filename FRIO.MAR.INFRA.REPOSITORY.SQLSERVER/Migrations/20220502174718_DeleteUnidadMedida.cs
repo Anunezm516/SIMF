658fe117f687b6ad0760ac2e61dc9df338efc7d5
Allyson Romina Nuñez Mecias <68491564+Anunezm516@users.noreply.github.com>
2022-05-02 12:57:01 -0500

    Mejoras migrate

---
+++ FRIO.MAR.INFRA.REPOSITORY.SQLSERVER/Migrations/20220502174718_DeleteUnidadMedida.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FRIO.MAR.INFRA.REPOSITORY.SQLSERVER.Migrations
+{
+    public partial class DeleteUnidadMedida : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "UnidadMedida");
+
+            migrationBuilder.DropColumn(
+                name: "UnidadMedida",
+                table: "ProductoCliente");
+
+            migrationBuilder.DropColumn(
+                name: "UnidadMedida",
+                table: "Producto");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UnidadMedida",
+                table: "ProductoCliente",
+                type: "varchar(50)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "UnidadMedida",
+                table: "Producto",
+                type: "varchar(50)",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "UnidadMedida",
+                columns: table => new
+                {
+                    UnidadMedidaId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Comentario = table.Column<string>(type: "varchar(max)", nullable: true),
+                    Estado = table.Column<bool>(type: "bit", nullable: true),
+                    Nombre = table.Column<string>(type: "varchar(max)", nullable: true),
+                    Simbolo = table.Column<string>(type: "varchar(max)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UnidadMedida", x => x.UnidadMedidaId);
+                });
+        }
+    }
+}
